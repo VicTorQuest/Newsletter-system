32cb4c3a930e3545cd80b870142bc705940be75b
Victor Okolie <victorokolie001@gmail.com>
2023-05-07 11:39:35 +0100

    made landing page

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.admin.views.decorators import staff_member_required
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect, render, get_object_or_404
+from django.template.loader import get_template
+from django.urls import reverse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'newsletter/home.html')
